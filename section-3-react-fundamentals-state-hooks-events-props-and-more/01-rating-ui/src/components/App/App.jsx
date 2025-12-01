32170f6afb7b2cd446b7e229c5e3387553a64b9c
Jakub Franěk <tofugangsw@gmail.com>
2025-12-02 00:30:51 +0100

    32. Props

--- section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/App/App.jsx
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/App/App.jsx
@@ -2,7 +2,13 @@ import Rating from "../Rating/Rating.jsx";
 
 function App() {
 
-  return <Rating />;
+    return (
+        <Rating
+            heading="How do you feel about React?"
+            color="red"
+            feedbackMessages={["Hate it", "Dislike it", "Meh", "Like it", "Love it"]}
+        />
+    );
 }
 
 export default App
--- section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 
-function Rating() {
+function Rating(
+    {
+        heading="Rate Your Experience",
+        color="gold",
+        feedbackMessages=["Terrible", "Poor", "Fair", "Good", "Excellent"]
+    }) {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
     const stars = Array.from({length: 5}, (_, index) => index + 1);
-    const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
     return(
         <div className="rating-container">
-            <h2>Rate Your Experience!</h2>
+            <h2>{heading}</h2>
             <div className="stars">
                 {stars.map((star) => (
                     <span
                         key={star}
-                        className={`star ${star <= (hover || rating) ? "active" : ""}`}
+                        className="star"
                         onClick={() => setRating(star)}
                         onMouseEnter={() => setHover(star)}
                         onMouseLeave={() => setHover(0)}
+                        style={{
+                            color: star <= (hover || rating) ? color : "#ccc"
+                        }}
                     >{"\u2605"}
                     </span>)
                 )}
