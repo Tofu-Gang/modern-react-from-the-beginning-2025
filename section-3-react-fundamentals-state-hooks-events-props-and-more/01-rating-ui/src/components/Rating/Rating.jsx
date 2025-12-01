17f928342763d4327407ad3d685e5a6e221151e9
Jakub Franěk <tofugangsw@gmail.com>
2025-12-01 22:43:18 +0100

    27. Handling Events

--- section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
@@ -1,11 +1,28 @@
 function Rating() {
     const stars = Array.from({length: 5}, (_, index) => index + 1);
 
+    function clicked(index) {
+        console.log("CLICKED!!!", index);
+    }
+
+    function hovered(direction, index) {
+        console.log(direction, index);
+    }
+
     return(
         <div className="rating-container">
             <h2>Rate Your Experience!</h2>
             <div className="stars">
-                {stars.map((star) => (<span key={star} className="star">{"\u2605"}</span>))}
+                {stars.map((star, index) => (
+                    <span
+                        key={star}
+                        className="star"
+                        onClick={() => clicked(index)}
+                        onMouseEnter={() => hovered("enter", index)}
+                        onMouseLeave={() => hovered("leave", index)}
+                    >{"\u2605"}
+                    </span>)
+                )}
             </div>
         </div>);
 }
