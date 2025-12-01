5a90241a1871fa3fb4231720ca9852047f17c966
Jakub Franěk <tofugangsw@gmail.com>
2025-12-02 00:57:24 +0100

    33. Component Composition

--- section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Star from "./Star.jsx";
 
 function Rating(
     {
@@ -8,24 +9,32 @@ function Rating(
     }) {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
-    const stars = Array.from({length: 5}, (_, index) => index + 1);
+    const stars = Array.from({length: feedbackMessages.length}, (_, index) => index + 1);
+
+    function setHoverEnter(star) {
+        setHover(star)
+    }
+
+    function setHoverLeave() {
+        setHover(0);
+    }
+
+    function getStarColor(star) {
+        return star <= (hover || rating) ? color : "#ccc";
+    }
 
     return(
         <div className="rating-container">
             <h2>{heading}</h2>
             <div className="stars">
                 {stars.map((star) => (
-                    <span
+                    <Star
                         key={star}
-                        className="star"
-                        onClick={() => setRating(star)}
-                        onMouseEnter={() => setHover(star)}
-                        onMouseLeave={() => setHover(0)}
-                        style={{
-                            color: star <= (hover || rating) ? color : "#ccc"
-                        }}
-                    >{"\u2605"}
-                    </span>)
+                        color={getStarColor(star)}
+                        setRating={() => setRating(star)}
+                        setHoverEnter={() => setHoverEnter(star)}
+                        setHoverLeave={setHoverLeave} />
+                    )
                 )}
             </div>
             { rating > 0 && <p className="feedback">{ feedbackMessages[rating - 1]}</p>}
---
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Star.jsx
@@ -0,0 +1,14 @@
+function Star({ color, setRating, setHoverEnter, setHoverLeave }) {
+    return (
+        <span
+            className="star"
+            onClick={setRating}
+            onMouseEnter={setHoverEnter}
+            onMouseLeave={setHoverLeave}
+            style={{ color: color }}
+        >{"\u2605"}
+        </span>
+    );
+}
+
+export default Star;
