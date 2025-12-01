7b1f6f0d92704bd4ce7ebf3060cb8929692d0bcb
Jakub Franěk <tofugangsw@gmail.com>
2025-12-02 00:19:59 +0100

    31. Conditional Rendering & Styling

--- section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
+++ section-3-react-fundamentals-state-hooks-events-props-and-more/01-rating-ui/src/components/Rating/Rating.jsx
@@ -4,6 +4,7 @@ function Rating() {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
     const stars = Array.from({length: 5}, (_, index) => index + 1);
+    const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
     return(
         <div className="rating-container">
@@ -12,7 +13,7 @@ function Rating() {
                 {stars.map((star) => (
                     <span
                         key={star}
-                        className="star"
+                        className={`star ${star <= (hover || rating) ? "active" : ""}`}
                         onClick={() => setRating(star)}
                         onMouseEnter={() => setHover(star)}
                         onMouseLeave={() => setHover(0)}
@@ -20,6 +21,7 @@ function Rating() {
                     </span>)
                 )}
             </div>
+            { rating > 0 && <p className="feedback">{ feedbackMessages[rating - 1]}</p>}
         </div>
     );
 }
