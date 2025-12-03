a9218aae0dc4f2a3a685ac86ce29cbd5896ea61a
Tofu Gang <tofugangsw@gmail.com>
2025-12-03 20:15:28 +0100

    Section 6: useRef Hook - Simple Timer Project

---
+++ simple-timer/src/components/App.jsx
@@ -0,0 +1,10 @@
+import Timer from "./simple-timer/Timer.jsx";
+
+function App() {
+
+  return (
+    <Timer />
+  )
+}
+
+export default App
---
+++ simple-timer/src/components/simple-timer/Controls.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+function Controls({ toggleTimer, isRunning, resetTimer}) {
+    const startButtonRef = useRef(null);
+    useEffect(() => {
+        if(startButtonRef.current) {
+            startButtonRef.current.focus();
+        }
+    }, [])
+
+    return (
+        <>
+            <button
+                onClick={toggleTimer}
+                className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3"
+                ref={startButtonRef}
+            >
+                {isRunning ? "Pause" : "Start"}
+            </button>
+            <button
+                onClick={resetTimer} className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+                Reset
+            </button>
+        </>
+    );
+}
+
+export default Controls;
