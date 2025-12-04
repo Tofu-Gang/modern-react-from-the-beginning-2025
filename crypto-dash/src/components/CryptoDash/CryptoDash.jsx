d06a56d1af1c59e816de5906b630cf861b0c8c96
Jakub Franěk <tofugangsw@gmail.com>
2025-12-04 15:35:50 +0100

    64. Display Coin Data

--- crypto-dash/src/components/CryptoDash/CryptoDash.jsx
+++ crypto-dash/src/components/CryptoDash/CryptoDash.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
+import CoinCard from "./CoinCard.jsx";
 
-const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=czk&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
 function CryptoDash() {
     const [coins, setCoins] = useState([]);
@@ -31,6 +32,15 @@ function CryptoDash() {
     return (
         <div>
             <h1>🚀Crypto Dash</h1>
+            {isLoading && <p>Loading...</p>}
+            {error && <div className="error">{error}</div>}
+            {!isLoading && !error && (
+                <main className="grid">
+                    {coins.map((coin) => (
+                        <CoinCard key={coin.id} coin={coin} />
+                    ))}
+                </main>
+            )}
         </div>
     );
 }
