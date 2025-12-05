2a0180d25e81b591241a6af1c41bbb86458639a6
Tofu Gang <tofugangsw@gmail.com>
2025-12-05 17:48:04 +0100

    72. About Page & Link Component

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,12 +1,18 @@
-import Home from "./pages/Home.jsx";
 import { Routes, Route } from "react-router";
+import Header from "./components/CryptoDash/Header.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
 
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </>
     );
 }
 
