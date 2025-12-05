ea0620b70feb93f871fc6eba66a0f756208e0b1f
Tofu Gang <tofugangsw@gmail.com>
2025-12-05 20:06:03 +0100

    75. Display Coin Details

--- crypto-dash/src/components/CryptoDash/CoinDetails.jsx
+++ crypto-dash/src/components/CryptoDash/CoinDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react";
-import { useParams } from "react-router";
+import {Link, useParams} from "react-router";
 
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
@@ -17,7 +17,6 @@ function CoinDetails() {
                     throw new Error("Failed to fetch data");
                 } else {
                     const data = await response.json();
-                    console.log(data);
                     setCoin(data);
                 }
             } catch(error) {
@@ -31,7 +30,42 @@ function CoinDetails() {
     }, [id])
 
     return (
-        <div>Coin Details {id}</div>
+        <div className="coin-details-container">
+            <Link to="/">⬅️ Go Back Home</Link>
+            <h1 className="coin-details-title">{coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : "Coin Details"}</h1>
+            { loading && <p>Loading...</p>}
+            { error && <div className="error">❌ {error}</div> }
+            { !loading && !error && (
+                <>
+                    <img src={coin.image.large} alt={coin.name} className="coin-details-image" />
+                    <p>{`${coin.description.en.split(". ")[0]}.`}</p>
+                    <div className="coin-details-info">
+                        <h3>#{coin.market_cap_rank}</h3>
+                        <h3>Current Price: {coin.market_data.current_price.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})}</h3>
+                        <h4>Market Cap: {coin.market_data.market_cap.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})}</h4>
+                        <h4>24h High: {coin.market_data.high_24h.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})}</h4>
+                        <h4>24h Low: {coin.market_data.low_24h.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})}</h4>
+                        <h4>24h Price Change: ${coin.market_data.price_change_24h.toFixed(2)} ({coin.market_data.price_change_percentage_24h.toFixed(2)}%)</h4>
+                        <h4>Circulating Supply: {coin.market_data.circulating_supply.toLocaleString("cs")}</h4>
+                        <h4>Total Supply: {coin.market_data?.total_supply.toLocaleString("cs") || "N/A"}</h4>
+                        <h4>All-Time High: {coin.market_data.ath.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})} on {new Date(coin.market_data.ath_date.czk).toLocaleDateString("cs")}</h4>
+                        <h4>All-Time Low: {coin.market_data.atl.czk.toLocaleString("cs", { style: "currency", currency: "CZK"})} on {new Date(coin.market_data.atl_date.czk).toLocaleDateString("cs")}</h4>
+                        <h4>Last Updated: { new Date(coin.last_updated).toLocaleDateString("cs")}</h4>
+                    </div>
+                    <div className="coin-details-links">
+                        {coin.links.homepage[0] && (
+                            <p>🌐 <a href={coin.links.homepage[0]} target="_blank" rel="noopener noreferer">Website</a></p>)}
+                        {coin.links.blockchain_site[0] && (
+                            <p>🧩 <a href={coin.links.blockchain_site[0]} target="_blank" rel="noopener noreferer">Blockchain Explorer</a></p>
+                        )}
+                        {coin.categories.length > 0 && (
+                            <p>Categories: {coin.categories.join(", ")}</p>
+                        )}
+                        {!loading && !error && !coin && <p>No Data Found!</p>}
+                    </div>
+                </>
+            )}
+        </div>
     );
 }
 
