944d2edb1bbb0c2cdc18cc36b9b23c61d6523e46
Tofu Gang <tofugangsw@gmail.com>
2025-12-08 17:33:31 +0100

    92. Header & Cart Count

---
+++ shopping-cart-ui/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useCart } from "../context/CartContext.jsx";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Header() {
+    const { cart } = useCart();
+    const itemCount = cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+
+    return (
+        <header className="bg-white shadow-md p-4 flex justify-between items-center">
+            <h1 className="text-2xl font-bold text-blue-600">
+                ShopMate
+            </h1>
+            <div className="relative">
+                <FaShoppingCart className="text-2xl text-gray-700" />
+                {itemCount > 0 && (
+                    <span
+                        className="
+                        absolute
+                        -top-2
+                        -right-2
+                        bg-red-500
+                        text-white
+                        text-xs
+                        w-5
+                        h-5
+                        flex
+                        items-center
+                        justify-center
+                        rounded-full"
+                    >{itemCount}</span>)}
+            </div>
+        </header>
+    );
+}
+
+export default Header;
