f82d8f6586c1bc6ed3ea996f555bd2941e197a0f
Tofu Gang <tofugangsw@gmail.com>
2025-12-08 16:53:37 +0100

    90. Cart Context

---
+++ shopping-cart-ui/src/context/CartContext.jsx
@@ -0,0 +1,13 @@
+import { useState, createContext, useContext } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    return <CartContext.Provider value={{ cart }} >{children}</CartContext.Provider>
+}
+
+export function useCart() {
+    return useContext(CartContext);
+}
