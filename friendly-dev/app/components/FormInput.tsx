a5724cac619b27c9ced733331548eaf93ac7832d
Tofu Gang <tofugangsw@gmail.com>
2025-12-10 19:25:47 +0100

    121. Form Validation With Actions

--- friendly-dev/app/components/FormInput.tsx
+++ friendly-dev/app/components/FormInput.tsx
@@ -1,10 +1,11 @@
 type FormInputProps = {
     label: string,
     inputType: string,
-    id: string
+    id: string,
+    error: string
 }
 
-function FormInput({ label, inputType, id }:FormInputProps) {
+function FormInput({ label, inputType, id, error }:FormInputProps) {
     let formInput;
 
     if (inputType === "text" || inputType === "email") {
@@ -17,6 +18,9 @@ function FormInput({ label, inputType, id }:FormInputProps) {
                     name={id}
                     className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
                 />
+                {error && (
+                    <p className="text-red-400 text-sm mt-1">{error}</p>
+                )}
             </div>
         );
     } else if (inputType === "textarea") {
@@ -28,6 +32,9 @@ function FormInput({ label, inputType, id }:FormInputProps) {
                     name={id}
                     className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
                 />
+                {error && (
+                    <p className="text-red-400 text-sm mt-1">{error}</p>
+                )}
             </div>
         );
     } else {
--- friendly-dev/app/routes/contact/index.tsx
+++ friendly-dev/app/routes/contact/index.tsx
@@ -13,33 +13,63 @@ const FORM_TEMPLATE = [
     {
         label: "Full Name",
         inputType: "text",
-        id: "name"
+        id: "name",
+        errorMessage: "Name is required"
     },
     {
         label: "Email",
         inputType: "email",
-        id: "email"
+        id: "email",
+        errorMessage: "Email is required",
+        regex: (/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
+        regexErrorMessage: "Invalid email format"
     },
     {
         label: "Subject",
         inputType: "text",
-        id: "subject"
+        id: "subject",
+        errorMessage: "Subject is required"
     },
     {
         label: "Message",
         inputType: "textarea",
-        id: "message"
+        id: "message",
+        errorMessage: "Message is required"
     }
 ];
 
 export async function action({ request }:Route.ActionArgs) {
     const formData = await request.formData();
     const ids = FORM_TEMPLATE.map((input_template) => input_template.id);
-    const data = ids.reduce((accumulator, id) => ({ ...accumulator, [id]: formData.get(id)}), {})
-    return { message: "Form submitted successfully!", data };
+    const data = ids.reduce((accumulator, id) => ({ ...accumulator, [id]: formData.get(id) as string}), {});
+    const validations = FORM_TEMPLATE.map((input_template) => ({
+        id: input_template.id,
+        errorMessage: input_template.errorMessage,
+        regex: input_template?.regex ? input_template.regex : null,
+        regexErrorMessage: input_template?.regexErrorMessage ? input_template.regexErrorMessage : null
+    }));
+    const errors = validations.reduce((accumulator, validation) => {
+        const a = {
+            ...accumulator,
+        };
+        if (!data[validation.id]) {
+            a[validation.id] = validation.errorMessage;
+        } else if (validation.regex !== null && !validation.regex.test(data[validation.id])) {
+            a[validation.id] = validation.regexErrorMessage;
+        }
+        return a;
+    }, {});
+
+    if (Object.keys(errors).length > 0) {
+        return { errors };
+    } else {
+        return { message: "Form submitted successfully!", data };
+    }
 }
 
 function ContactPage({ actionData }:Route.ComponentProps) {
+    const errors = actionData?.errors || {};
+
     return (
         <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8 text-center">📬Contact Me</h2>
@@ -48,7 +78,7 @@ function ContactPage({ actionData }:Route.ComponentProps) {
             ) : null}
             <Form method="POST" className="space-y-6">
                 {FORM_TEMPLATE.map((data) => (
-                    <FormInput key={data.id} label={data.label} inputType={data.inputType} id={data.id} />
+                    <FormInput key={data.id} label={data.label} inputType={data.inputType} id={data.id} error={errors[data.id]} />
                 ))}
                 <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">
                     Send Message
