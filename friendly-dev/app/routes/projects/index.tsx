de754dd29c9d33d56824b9a018c617c2aa3ae50f
Jakub Franěk <tofugangsw@gmail.com>
2025-12-10 13:15:32 +0100

    116. Animate Project Filter

--- friendly-dev/app/routes/projects/index.tsx
+++ friendly-dev/app/routes/projects/index.tsx
@@ -4,6 +4,7 @@ import type { Project } from "~/types";
 import ProjectCard from "../../components/ProjectCard";
 import Pagination from "../../components/Pagination";
 import Categories from "../../components/Categories";
+import { AnimatePresence, motion } from "framer-motion";
 
 // server loader
 export async function loader({ request }:Route.LoaderArgs):Promise<{ projects: Project[] }> {
@@ -40,9 +41,15 @@ function ProjectsPage({ loaderData }: Route.ComponentProps) {
         <>
             <h2 className="text-3xl font-bold text-white mb-8">🚀Projects</h2>
             <Categories categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} setCurrentPage={setCurrentPage} />
-            <div className="grid gap-6 sm:grid-cols-2">
-                {currentProjects.map((project) => <ProjectCard key={project.id} project={project} />)}
-            </div>
+            <AnimatePresence mode="wait">
+                <motion.div layout className="grid gap-6 sm:grid-cols-2">
+                    {currentProjects.map((project) => (
+                        <motion.div key={project.id} layout>
+                            <ProjectCard key={project.id} project={project} />
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </AnimatePresence>
             <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
         </>
     );
