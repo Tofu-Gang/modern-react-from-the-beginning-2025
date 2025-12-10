bccb962f28ae59a0bdd31bf4038c3a29b1e9f7ba
Jakub Franěk <tofugangsw@gmail.com>
2025-12-10 12:42:48 +0100

    114. Pagination Component

--- friendly-dev/app/routes/projects/index.tsx
+++ friendly-dev/app/routes/projects/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import ProjectCard from "../../components/ProjectCard";
+import Pagination from "../../components/Pagination";
 
 // server loader
 export async function loader({ request }:Route.LoaderArgs):Promise<{ projects: Project[] }> {
@@ -20,28 +21,12 @@ export function meta({}: Route.MetaArgs) {
 function ProjectsPage({ loaderData }: Route.ComponentProps) {
     const { projects } = loaderData as { projects: Project[] };
     const [currentPage, setCurrentPage] = useState(1);
-    const projectsPerPage = 2;
+    const projectsPerPage = 10;
     const totalPages = Math.ceil(projects.length / projectsPerPage);
     // Get current page projects
     const indexOfLast = currentPage * projectsPerPage;
     const indexOfFirst = indexOfLast - projectsPerPage;
     const currentProjects = projects.slice(indexOfFirst, indexOfLast);
-
-    function renderPagination() {
-        return (
-            <div className="flex justify-center gap-2 mt-8">
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        className={`px-3 py-1 rounded ${currentPage === index + 1 ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-200 cursor-pointer"}`}
-                        onClick={() => setCurrentPage(index + 1)}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-            </div>
-        );
-    }
 
     return(
         <>
@@ -49,7 +34,7 @@ function ProjectsPage({ loaderData }: Route.ComponentProps) {
             <div className="grid gap-6 sm:grid-cols-2">
                 {currentProjects.map((project) => <ProjectCard key={project.id} project={project} />)}
             </div>
-            {totalPages > 1 && renderPagination()}
+            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
         </>
     );
 }
