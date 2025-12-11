e3d3c0da23ae09ff0bf3de657ae73f4d655d4f0e
Jakub Franěk <tofugangsw@gmail.com>
2025-12-11 18:20:33 +0100

    129. Re-Use Pagination Component

--- friendly-dev/app/routes/blog/index.tsx
+++ friendly-dev/app/routes/blog/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import PostCard from "../../components/PostCard";
+import Pagination from "../../components/Pagination";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 
@@ -22,11 +24,21 @@ export async function loader({ request }:Route.LoaderArgs):Promise<{ posts: Post
 }
 
 function BlogPage({ loaderData }:Route.ComponentProps) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 10;
     const { posts } = loaderData;
+    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
     return (
         <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8">📝Blog</h2>
-            {posts.map((post) => <PostCard key={post.slug} post={post} />)}
+            {currentPosts.map((post) => <PostCard key={post.slug} post={post} />)}
+            {totalPages > 1 && (
+                <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
+            )}
         </div>
     );
 }
