e4809a3755771a1a6d2d52f13086a4db12b5a35c
Tofu Gang <tofugangsw@gmail.com>
2025-12-14 22:58:07 +0100

    156. React Query Devtools Install

--- github-finder/src/main.tsx
+++ github-finder/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient} >
         <StrictMode>
             <App/>
+            <ReactQueryDevtools initialIsOpen={false} position="bottom" />
         </StrictMode>
     </QueryClientProvider>
 );
