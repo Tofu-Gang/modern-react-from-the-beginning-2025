3af1751929b402b41831c0e7f92779648769e13f
Tofu Gang <tofugangsw@gmail.com>
2025-12-17 20:08:45 +0100

    174. Fetch Data With TanStack Router Loaders

--- idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
+++ idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,21 @@
 import {createFileRoute} from '@tanstack/react-router'
 
+async function fetchIdea(ideaId:string) {
+    const response = await fetch(`http://localhost:8000/ideas/${ideaId}`);
+
+    if(!response.ok) {
+        throw new Error(`Failed to fetch idea ${ideaId}`);
+    } else {
+        return await response.json();
+    }
+}
+
 export const Route = createFileRoute('/ideas/$ideaId/')({
     component: IdeaDetailsPage,
+    loader: async ({ params }) => fetchIdea(params.ideaId)
 })
 
 function IdeaDetailsPage() {
-    return <div>Hello "/ideas/$ideaId/"!</div>
+    const idea = Route.useLoaderData();
+    return <div>{idea.title}</div>
 }
