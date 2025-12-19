b3d1c4dce135b39e96cf509cf6e1002c787f0ba6
Tofu Gang <tofugangsw@gmail.com>
2025-12-19 18:59:45 +0100

    183. IdeaCard Component

--- idea-drop/idea-drop-ui/src/routes/index.tsx
+++ idea-drop/idea-drop-ui/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { fetchIdeas } from "@/api/ideas.ts";
 import { createFileRoute, Link } from '@tanstack/react-router'
 import type { Idea } from "@/types.ts";
 import { Lightbulb } from "lucide-react";
+import IdeaCard from "@/components/IdeaCard.tsx";
 
 function ideasQueryOptions() {
     return queryOptions({
@@ -44,21 +45,11 @@ function HomePage() {
 
             <section className="flex-1">
                 <h2 className="text-2xl font-semibold mb-4 text-gray-800">Latest Ideas</h2>
-                <ul className="space-y-6">
+                <div className="space-y-6">
                     {latestIdeas.map((idea) => (
-                        <li key={idea.id} className="border border-gray-300 rounded-lg shadow p-4 bg-white">
-                            <h3 className="text-lg font-bold text-gray-900">{idea.title}</h3>
-                            <p className="text-gray-600 mb-2">{idea.summary}</p>
-                            <Link
-                                to="/ideas/$ideaId"
-                                params={{ideaId: idea.id.toString()}}
-                                className="text-blue-600 hover:underline"
-                            >
-                                Read more →
-                            </Link>
-                        </li>
+                        <IdeaCard key={idea.id} idea={idea} button={false} />
                     ))}
-                </ul>
+                </div>
                 <div className="mt-6">
                     <Link to="/ideas" className="w-full text-center inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-2 rounded-md transition">
                         View All Ideas
