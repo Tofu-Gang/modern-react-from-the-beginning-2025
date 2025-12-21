0233d82ecd5193ec547b214c9981952f379511a3
Jakub Franěk <tofugangsw@gmail.com>
2025-12-21 16:22:44 +0100

    194. Route Files

---
+++ idea-drop/idea-drop-api/routes/ideaRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const router = express.Router();
+
+// @route           GET /api/ideas
+// @description     Get all ideas
+// @access          Public
+router.get("/", (request, response) => {
+    const ideas = [
+        {id: 1, title: "Idea 1", description: "This is idea 1"},
+        {id: 2, title: "Idea 2", description: "This is idea 2"},
+        {id: 3, title: "Idea 3", description: "This is idea 3"},
+    ];
+    response.json(ideas);
+});
+
+// @route           POST /api/ideas
+// @description     Create new idea
+// @access          Public
+router.post("/", (request, response) => {
+    const { title, description } = request.body;
+    response.send(`<h1>Processed: ${title}</h1><p>${description}</p>`);
+});
+
+export default router;
--- idea-drop/idea-drop-api/server.js
+++ idea-drop/idea-drop-api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes/ideaRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -10,18 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/ideas", (request, response) => {
-    const ideas = [
-        {id: 1, title: "Idea 1", description: "This is idea 1"},
-        {id: 2, title: "Idea 2", description: "This is idea 2"},
-        {id: 3, title: "Idea 3", description: "This is idea 3"},
-    ];
-    response.json(ideas);
-});
-
-app.post("/api/ideas", (request, response) => {
-    const { title, description } = request.body;
-    response.send(`<h1>Processed: ${title}</h1><p>${description}</p>`);
-});
+app.use("/api/ideas", router);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
