3bb440e3278f21eb0b70eb335f121b2d39178bb5
Jakub Franěk <tofugangsw@gmail.com>
2025-12-21 15:29:39 +0100

    193. Creating Server Edpoints

--- idea-drop/idea-drop-api/server.js
+++ idea-drop/idea-drop-api/server.js
@@ -7,5 +7,21 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/ideas", (request, response) => {
+    const ideas = [
+        {id: 1, title: "Idea 1", description: "This is idea 1"},
+        {id: 2, title: "Idea 2", description: "This is idea 2"},
+        {id: 3, title: "Idea 3", description: "This is idea 3"},
+    ];
+    response.json(ideas);
+});
+
+app.post("/api/ideas", (request, response) => {
+    const { title, description } = request.body;
+    response.send(`<h1>Processed: ${title}</h1><p>${description}</p>`);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
