bf4fb44a851c73026722c898ad60cf30d9dd16c5
Jakub Franěk <tofugangsw@gmail.com>
2025-12-27 01:21:40 +0100

    208. Generate Access & Refresh Tokens

--- idea-drop/idea-drop-api/routes/authRoutes.js
+++ idea-drop/idea-drop-api/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
+import { generateToken } from "../utils/generateToken.js";
 
 const router = express.Router();
 
@@ -21,8 +22,20 @@ router.post("/register", async(request, response, next) => {
                 throw new Error("User already exists!");
             } else {
                 const user = await User.create({ name, email, password });
+                // Create Tokens
+                const payload = { userId: user._id.toString() };
+                const accessToken = await generateToken(payload, "1m");
+                const refreshToken = await generateToken(payload, "30d");
+                // Set refresh token in HTTP-Only cookie
+                response.cookie("refreshToken", refreshToken, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: "none",
+                    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+                });
 
                 response.status(201).json({
+                    accessToken,
                     user: {
                         id: user._id,
                         name: user.name,
