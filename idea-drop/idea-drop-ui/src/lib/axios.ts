c69bf811475f392250b78952206ab47c47467c2f
Jakub Franěk <tofugangsw@gmail.com>
2026-01-04 18:39:41 +0100

    223. Refresh Token When It Expires

--- idea-drop/idea-drop-ui/src/lib/axios.ts
+++ idea-drop/idea-drop-ui/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getStoredAccessToken } from "@/lib/authToken.ts";
+import { getStoredAccessToken, setStoredAccessToken } from "@/lib/authToken.ts";
+import { refreshAccessToken } from "@/api/auth.ts";
 
 const api = axios.create({
     baseURL: "/api",
@@ -9,6 +10,7 @@ const api = axios.create({
     }
 });
 
+// attach token on refresh
 api.interceptors.request.use((config) => {
     const token = getStoredAccessToken();
 
@@ -17,6 +19,26 @@ api.interceptors.request.use((config) => {
     }
 
     return config;
+});
+
+// refresh token after expire
+api.interceptors.response.use((response) => response, async (error) => {
+    const originalRequest = error.config;
+
+    if(error.response?.status === 401 && !originalRequest._retry && !originalRequest.url.includes("/auth/refresh")) {
+        originalRequest._retry = true;
+
+        try {
+            const { accessToken: newToken } = await refreshAccessToken();
+            setStoredAccessToken(newToken);
+            originalRequest.headers.Authorization = `Bearer ${newToken}`;
+            return api(originalRequest);
+        } catch(error) {
+            console.error("Refresh token failed", error);
+        }
+    } else {
+        return Promise.reject(error);
+    }
 });
 
 export default api;
